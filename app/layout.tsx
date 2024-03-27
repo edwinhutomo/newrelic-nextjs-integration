5efb0146bb0094c7d32448b41595ac36832f96b3
Edwin Hutomo <edwin.hutomo@gojek.com>
2024-03-27 14:52:37 +0800

    Add browser monitoring script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import newrelic from 'newrelic';
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +16,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const browserTimingHeader = newrelic.getBrowserTimingHeader({
+    hasToRemoveScriptWrapper: true,
+  });
   return (
     <html lang="en">
+    <Head>
+      <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{ __html: browserTimingHeader }}
+      />
+    </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
